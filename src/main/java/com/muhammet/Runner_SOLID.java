ac2915f0bf44db6e478325e2d66ed0920e0665ab
bilgejava6 <bilgejava6@gmail.com>
2024-12-17 21:50:14 +0300

    SOLID e giriş yapıldı.

---
+++ src/main/java/com/muhammet/Runner_SOLID.java
@@ -0,0 +1,34 @@
+package com.muhammet;
+
+public class Runner_SOLID {
+    public static void main(String[] args) {
+        /**
+         * SOLID, ACID, AGILE, Design Patterns
+         *
+         * Amaç,
+         * 1- Kodlamanın esnek ve geliştirilebilir olmasını sağlamak,
+         * 2- Kodlarınız daha anlaşılır ve okunaklı olur,
+         * 3- Kodlarımız modüler olur, böylelikle esneye bildiği gibi
+         * kodlarımız başka projlerde de kullanılabilir olur,
+         * 4- Kodların test edilebilirliği artar,
+         * 5- SOLID, kodlama maliyetlerinizi düşürür, zamandan tasarruf edersiniz.
+         * -------------
+         *
+         * S - Single Responsibility Principle (Tek Sorumluluk İlkesi)
+         * Interface, Class, Method gibi kavramların kendi gövleri ile iligili alanlarda
+         * kalmaları ve tek iş yapmalarının sağlanması gereklidir.
+         * IUserRepository -> User entity sinin DB yönetimini yürüten interface
+         * ICrud, IService, UserService
+         * save(), findAllByUserNameAndPassword(), getAllByName()
+         *
+         * O -
+         *
+         * L
+         *
+         * I
+         *
+         * D
+         *
+         */
+    }
+}
